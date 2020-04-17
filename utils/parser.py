57a46a2c74ff500d7c8b47d5f8df02795496eea3
Ayuei <sefton_student@hotmail.com>
2020-04-17 14:00:57 +1000

    Added parser for COVID corpus

---
+++ utils/parser.py
@@ -0,0 +1,44 @@
+import json
+
+class Parser:
+    @classmethod
+    def parse_document(cls, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class CovidParser(Parser):
+    is_str = lambda k: isinstance(k, str)
+
+    @classmethod
+    def parse(cls, row):
+        document = {'id': row['cord_uid']}
+
+        if cls.is_str(row['full_text_file']):
+            path = None
+            if row['has_pmc_xml_parse']:
+                path = 'dataset-week1/' + row['full_text_file'] + \
+                       '/pmc_json/' + row['pmcid'].split(';')[ 0] +\
+                       '.xml.json'
+
+            if row['has_pdf_parse']:
+                path = 'dataset-week1/' + row['full_text_file'] +\
+                       '/pdf_json/' + row['sha'].split(';')[0] +\
+                       '.json'
+
+            if path:
+                content = json.load(open(path))
+                fulltext = '\n'.join([p['text'] for p in content['body_text']])
+                document['fulltext'] = fulltext
+        else:
+            document['fulltext'] = ''
+
+        if cls.is_str(row['abstract']):
+            document['abstract']=row['abstract']
+        else:
+            document['abstract']=''
+        if cls.is_str(row['publish_time']):
+            date = row['publish_time']
+            len_is_4 = len(row['publish_time']) == 4
+            document['date'] = f'{date}-01-01' if len_is_4 else date
+
+        return document

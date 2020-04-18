17d48e4b1018191f51a1d1ed2d1e066949641704
Vincent Nguyen <Vincent.Nguyen@anu.edu.au>
2020-04-19 00:32:06 +1000

    Environment script for Bracewell

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source venv/bin/activate
+
+module load tensorflow/1.12.0-py37-gpu
+module load pytorch/1.4.0-py37-cuda90
+module load protobuf/3.6.1
+alias vim=vi
+
+
+nohup bert-serving-start -model_dir biobert_nli/model_tf/ -pooling_layer -1 -pooling_strategy CLS_TOKEN -port 51234 -port_out 51235 -max_seq_len None > bert_serving.nohup 2>&1 &
+nohup ./elasticsearch-7.6.2/bin/elasticsearch > es_server.nohup 2>&1 &
